00579aafd53746483d9cff8cb6b0fc58fc340eb8
dung tran <llawliet13032001@gmail.com>
2021-11-23 22:34:22 +0700

    change title

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          change 1 <code>src/App.js</code> and save to reload.
+          change 2 <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
